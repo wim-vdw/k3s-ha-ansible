8b6f5c3a2ff11d43a545deab47e2fd0bdad4f53e
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2025-09-21 12:37:34 +0200

    add Proxmox scripts

---
+++ proxmox/k3s-stop-all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Stopping K3S server nodes"
+qm start 100
+qm start 101
+qm start 102
+
+echo "Stopping K3S worker nodes"
+qm start 200
+qm start 201
+
+echo "Stopping HAProxy load balancer nodes"
+qm start 300
+qm start 301
