6c15b63033d44bc622fcaf423f7eaa559715aa18
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2025-09-21 13:02:02 +0200

    add Proxmox scripts

--- proxmox/k3s-create-all.sh
+++ proxmox/k3s-create-all.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+  echo "Usage: $0 <path_to_id_rsa.pub>"
+  exit 1
+fi
+
+PUBKEY="$1"
+
 qm clone 9000 100 --name k3s-server-01
 qm resize 100 scsi0 32G
 qm set 100 --net0 virtio,bridge=vmbr0,macaddr=BC:24:11:66:6F:07
@@ -7,7 +14,7 @@ qm set 100 --cpu host
 qm set 100 --memory 4096
 qm set 100 --cores 2
 qm set 100 --ciuser wim
-qm set 100 --sshkey id_rsa.pub
+qm set 100 --sshkey "$PUBKEY"
 qm set 100 --ciupgrade 1
 qm set 100 --ipconfig0 ip=dhcp
 
@@ -18,7 +25,7 @@ qm set 101 --cpu host
 qm set 101 --memory 4096
 qm set 101 --cores 2
 qm set 101 --ciuser wim
-qm set 101 --sshkey id_rsa.pub
+qm set 101 --sshkey "$PUBKEY"
 qm set 101 --ciupgrade 1
 qm set 101 --ipconfig0 ip=dhcp
 
@@ -29,7 +36,7 @@ qm set 102 --cpu host
 qm set 102 --memory 4096
 qm set 102 --cores 2
 qm set 102 --ciuser wim
-qm set 102 --sshkey id_rsa.pub
+qm set 102 --sshkey "$PUBKEY"
 qm set 102 --ciupgrade 1
 qm set 102 --ipconfig0 ip=dhcp
 
@@ -40,7 +47,7 @@ qm set 200 --cpu host
 qm set 200 --memory 8192
 qm set 200 --cores 4
 qm set 200 --ciuser wim
-qm set 200 --sshkey id_rsa.pub
+qm set 200 --sshkey "$PUBKEY"
 qm set 200 --ciupgrade 1
 qm set 200 --ipconfig0 ip=dhcp
 
@@ -51,7 +58,7 @@ qm set 201 --cpu host
 qm set 201 --memory 8192
 qm set 201 --cores 4
 qm set 201 --ciuser wim
-qm set 201 --sshkey id_rsa.pub
+qm set 201 --sshkey "$PUBKEY"
 qm set 201 --ciupgrade 1
 qm set 201 --ipconfig0 ip=dhcp
 
@@ -62,7 +69,7 @@ qm set 300 --cpu host
 qm set 300 --memory 2048
 qm set 300 --cores 1
 qm set 300 --ciuser wim
-qm set 300 --sshkey id_rsa.pub
+qm set 300 --sshkey "$PUBKEY"
 qm set 300 --ciupgrade 1
 qm set 300 --ipconfig0 ip=dhcp
 
@@ -73,6 +80,6 @@ qm set 301 --cpu host
 qm set 301 --memory 2048
 qm set 301 --cores 1
 qm set 301 --ciuser wim
-qm set 301 --sshkey id_rsa.pub
+qm set 301 --sshkey "$PUBKEY"
 qm set 301 --ciupgrade 1
 qm set 301 --ipconfig0 ip=dhcp
