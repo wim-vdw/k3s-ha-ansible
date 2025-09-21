0c10519547da7877e00e721195971c92adfba41e
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2025-09-21 12:43:42 +0200

    add Proxmox scripts

--- proxmox/k3s-destroy-all.sh
+++ proxmox/k3s-destroy-all.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
 echo "Destroying K3S server nodes"
+qm stop 100
 qm destroy 100 --purge
+qm stop 101
 qm destroy 101 --purge
+qm stop 102
 qm destroy 102 --purge
 
 echo "Destroying K3S worker nodes"
+qm stop 200
 qm destroy 200 --purge
+qm stop 201
 qm destroy 201 --purge
 
 echo "Destroying HAProxy load balancer nodes"
+qm stop 300
 qm destroy 300 --purge
+qm stop 301
 qm destroy 301 --purge
